59980a0fc210255eae77fb67ef9eb00c747b4926
Mauricio Nunes <mutila@gmail.com>
2022-09-30 15:24:50 -0300

    secound commit

---
+++ freebsd_desktop.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+
+pkg_basic()
+{
+    pkg update -y
+    pkg upgrade -y
+    pkg install xorg
+}
+
+edit_rc()
+{
+    echo 'moused_enable="YES"' >> /etc/rc.conf
+    echo 'dbus_enable="YES"' >> /etc/rc.conf
+    echo 'hald_enable="YES"' >> /etc/rc.conf
+    echo 'sound_load="YES"' >> /etc/rc.conf
+    echo 'snd_hda_load="YES"' >> /etc/rc.conf
+}
+
+edit_fstab()
+{
+    echo 'proc  /proc   procfs  rw  0   0'
+}
+
+gnome4()
+{
+    echo "Starting Gnome4 Installer"
+    pkg_basic
+    pkg install gnome-42_2 gnome-desktop-42.2 gdm-42.0_2 -y
+    edit_rc
+    edit_fstab
+    echo 'gnome_enable="YES"' >> /etc/rc.conf
+    echo 'gdm_enable="YES"' >> /etc/rc.conf
+}
+
+kde_plasma()
+{
+    echo "Starting KDE Plasma Installer"
+    pkg_basic
+    pkg install x11/kde5 x11/sddm -y
+    edit_rc
+    edit_fstab
+    echo 'sddm_enable="YES"' >> /etc/rc.conf
+
+}
+
+xfce()
+{
+    echo "Starting XFCE Installer"
+    pkg_basic
+    pkg install xfce slim slim-themes -y
+    echo 'slim_enable="YES"' >> /etc/rc.conf
+}
+
+cria_xinit()
+{
+    declare -a USER_DIR
+    USER_DIR=$(ls -d /home/*)
+    for usuario in $USER_DIR
+    do
+        TEMP_USER=$(sed "s/\/home\///g" $usuario)
+        echo $TEMP_USER
+    done
+}
+
+CHOICE=0
+
+while [ $CHOICE -ne 4 ]; do
+
+    CHOICE=$(dialog --backtitle "Desktop Enviroment Installer" --title "Select Enviroments" --menu "Este é um script com intuito de facilitar a vida do usuario iniciente que queira testar o FreeBSD como Desktop" 15 40 20 1 "Gnome" 2 "KDE Plasma" 3 "XFCE" 4 "Sair" 2>&1 > /dev/tty )
+
+    clear
+    case $CHOICE in
+        1)
+            gnome4
+            ;;
+        2)
+            kde_plasma
+            ;;
+        3)
+            xfce
+            ;;
+    esac
+done
