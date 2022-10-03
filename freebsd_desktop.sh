af937dd39b53d40f9e690041b445ebf1234b89dd
Mauricio Nunes <mutila@gmail.com>
2022-10-03 15:31:48 -0300

    fix

--- freebsd_desktop.sh
+++ freebsd_desktop.sh
@@ -81,7 +81,7 @@ cria_xinit()
 
 apps_list()
 {
-    $apps=$(dialog --stdout --checklist 'Quais aplicativos deseja instalar?' 0    0   0 \
+    apps=$(dialog --stdout --checklist 'Quais aplicativos deseja instalar?' 0 0 0 \
     firefox ''  OFF \
     wifimgr ''  OFF \
     thunderbird ''  OFF \
