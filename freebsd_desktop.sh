b56840a77bde2dbf38c99f6829178f1e8da675c1
Mauricio Nunes <mutila@gmail.com>
2022-10-04 08:51:04 -0300

    new features

--- freebsd_desktop.sh
+++ freebsd_desktop.sh
@@ -65,27 +65,17 @@ xfce()
     sysrc 'lightdm_enable="YES"'
 }
 
-cria_xinit()
-{
-    declare -a USER_DIR
-    USER_DIR=$(ls -d /home/*)
-    for usuario in $USER_DIR
-    do
-        TEMP_USER=$(sed "s/\/home\///g" $usuario)
-        echo $TEMP_USER
-    done
-}
-
 apps_list()
 {
-    apps=$(dialog --stdout --checklist 'Quais aplicativos deseja instalar?' 0 0 0 \
+    apps=$(dialog --stdout --checklist 'Which apps do you want to install?' 0 0 0 \
     firefox ''  OFF \
     wifimgr ''  OFF \
     thunderbird ''  OFF \
     wine    ''  OFF \
     wine-gecko  ''  OFF \
     wine-mono   ''  OFF \
-    libreoffice ''  OFF )
+    libreoffice ''  OFF \
+    amdgpu '' OFF)
     pkg install -y $apps
 }
 
@@ -95,7 +85,7 @@ CHOICE=0
 
 while [ $CHOICE -ne 5 ]; do
 
-    CHOICE=$(dialog --backtitle "Desktop Enviroment Installer" --title "Select Enviroments" --menu "Este é um script com intuito de facilitar a vida do usuario iniciente que queira testar o FreeBSD como Desktop" 15 40 20 1 "Gnome" 2 "KDE Plasma" 3 "XFCE" 4 "Aplicativos" 5 "Sair" 2>&1 > /dev/tty )
+    CHOICE=$(dialog --backtitle "Desktop Enviroment Installer" --title "Select Enviroments" --menu "This is a script to make life easier for the novice user who wants to test FreeBSD as a Desktop" 15 40 20 1 "Gnome" 2 "KDE Plasma" 3 "XFCE" 4 "Apps" 5 "Sair" 2>&1 > /dev/tty )
 
     clear
     case $CHOICE in
