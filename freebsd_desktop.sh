4e98fd8f32b655a434a93f0c6a93efde58df437e
Mauricio Nunes <mutila@gmail.com>
2022-12-23 11:45:30 -0300

    i forgot add i3 in menu

--- freebsd_desktop.sh
+++ freebsd_desktop.sh
@@ -245,7 +245,7 @@ menu()
 
     while [ $CHOICE -ne 8 ]; do
 
-        CHOICE=$(dialog --backtitle "Desktop Enviroment Installer" --title "Select Enviroments" --menu "This is a script to make life easier for the novice user who wants to test FreeBSD as a Desktop" 15 40 20 1 "Gnome" 2 "Kde Plasma" 3 "Xfce" 4 "Mate" 5 "Window Maker" 6 "Lumina" 7 "Quit" 2>&1 > /dev/tty)
+        CHOICE=$(dialog --backtitle "Desktop Enviroment Installer" --title "Select Enviroments" --menu "This is a script to make life easier for the novice user who wants to test FreeBSD as a Desktop" 15 40 20 1 "Gnome" 2 "Kde Plasma" 3 "Xfce" 4 "Mate" 5 "Window Maker" 6 "Lumina" 7 "i3" 8 "Quit" 2>&1 > /dev/tty)
         
         clear
         case $CHOICE in
